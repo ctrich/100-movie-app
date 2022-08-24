51558b45e6482d3e29a60010c7e2eda4f7e75ad1
ctrich <ctrich86@yahoo.com>
2022-08-24 11:36:29 -0700

    URLs

---
+++ config/URL.js
@@ -0,0 +1,5 @@
+const BASE_URL = "https://api.themoviedb.org/3/";
+const POPULAR_MOVIES = "movie/popular?api_key=";
+const IMAGE_URL = "https://image.tmdb.org/t/p/";
+
+module.exports = { BASE_URL, POPULAR_MOVIES, IMAGE_URL };
