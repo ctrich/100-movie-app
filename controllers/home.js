735e4fa1344bf2a2d48c47498171c5092717bafd
ctrich <ctrich86@yahoo.com>
2022-08-24 11:35:23 -0700

    testing api calls

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,17 @@
+const axios = require("axios");
+const { BASE_URL, POPULAR_MOVIES, IMAGE_URL } = require("../config/URL");
+require("dotenv").config({ path: "../config/.env" });
+
 module.exports = {
   getIndex: (req, res) => {
-    res.render("index.ejs");
+    axios
+      .get(`${BASE_URL}${POPULAR_MOVIES}${process.env.TMDB_API_KEY}`)
+      .then((data) => {
+        res.render("index.ejs", {
+          popularMovies: data.data.results,
+          imageURL: IMAGE_URL,
+        });
+      })
+      .catch((err) => console.log(err));
   },
 };
