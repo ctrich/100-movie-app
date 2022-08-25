a62e1df4eceead2c8a9fcaae5104030f6aea1407
ctrich <ctrich86@yahoo.com>
2022-08-25 14:19:54 -0700

    added functionality to the image carousel for larger screens

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,6 +1,9 @@
 //Toggle navigation on mobile
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const nav = document.querySelector(".primary-navigation");
+const leftArrow = document.querySelector(".left-arrow");
+let rightArrow = document.querySelector(".right-arrow");
+let slideNumber = 0;
 
 navToggle.addEventListener("click", () => {
   const visibility = nav.getAttribute("data-visible");
@@ -11,5 +14,38 @@ navToggle.addEventListener("click", () => {
   } else {
     nav.setAttribute("data-visible", false);
     navToggle.setAttribute("aria-expanded", false);
+  }
+});
+
+leftArrow.addEventListener("click", () => {
+  if (slideNumber > 0) {
+    console.log("should be moving");
+    document
+      .querySelector(".carousel")
+      .animate(
+        [
+          { transform: `translateX(${slideNumber * -100}%)` },
+          { transform: `translateX(${(slideNumber - 1) * -100}%)` },
+        ],
+        { duration: 400, fill: "forwards" }
+      );
+    slideNumber--;
+  }
+});
+
+rightArrow.addEventListener("click", () => {
+  if (slideNumber < 9) {
+    console.log("should be moving");
+    document
+      .querySelector(".carousel")
+      .animate(
+        [
+          { transform: `translateX(${slideNumber * -100}%)` },
+          { transform: `translateX(${(slideNumber + 1) * -100}%)` },
+        ],
+        { duration: 400, fill: "forwards" }
+      );
+
+    slideNumber++;
   }
 });
