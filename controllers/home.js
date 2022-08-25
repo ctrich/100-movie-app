501f1af5505ff11d3273e8bee4a5fc5a0948354e
ctrich <ctrich86@yahoo.com>
2022-08-25 14:19:14 -0700

    changed GET requests to a different api endpoint

--- controllers/home.js
+++ controllers/home.js
@@ -1,14 +1,14 @@
 const axios = require("axios");
-const { BASE_URL, POPULAR_MOVIES, IMAGE_URL } = require("../config/URL");
+const { BASE_URL, NOW_PLAYING_MOVIES, IMAGE_URL } = require("../config/URL");
 require("dotenv").config({ path: "../config/.env" });
 
 module.exports = {
   getIndex: (req, res) => {
     axios
-      .get(`${BASE_URL}${POPULAR_MOVIES}${process.env.TMDB_API_KEY}`)
+      .get(`${BASE_URL}${NOW_PLAYING_MOVIES}${process.env.TMDB_API_KEY}`)
       .then((data) => {
         res.render("index.ejs", {
-          popularMovies: data.data.results,
+          nowPlaying: data.data.results,
           imageURL: IMAGE_URL,
         });
       })
