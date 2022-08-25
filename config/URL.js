a5eb1c42eea08c9bb09f3ef53351770d11b03a4f
ctrich <ctrich86@yahoo.com>
2022-08-25 14:18:28 -0700

    added more  URLs

--- config/URL.js
+++ config/URL.js
@@ -1,5 +1,13 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
 const POPULAR_MOVIES = "movie/popular?api_key=";
 const IMAGE_URL = "https://image.tmdb.org/t/p/";
+const UPCOMING_MOVIES = "movie/upcoming?api_key=";
+const NOW_PLAYING_MOVIES = "movie/now_playing?api_key=";
 
-module.exports = { BASE_URL, POPULAR_MOVIES, IMAGE_URL };
+module.exports = {
+  BASE_URL,
+  POPULAR_MOVIES,
+  IMAGE_URL,
+  UPCOMING_MOVIES,
+  NOW_PLAYING_MOVIES,
+};
