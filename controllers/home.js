56bd1ecc89c22862c4b460029068c199f1bbf2ce
ctrich <ctrich86@yahoo.com>
2022-08-29 12:43:56 -0700

    Changed axios request to make multiple requests at once

--- controllers/home.js
+++ controllers/home.js
@@ -1,17 +1,41 @@
 const axios = require("axios");
-const { BASE_URL, NOW_PLAYING_MOVIES, IMAGE_URL } = require("../config/URL");
+const {
+  BASE_URL,
+  NOW_PLAYING_MOVIES,
+  UPCOMING_MOVIES,
+  IMAGE_URL,
+} = require("../config/URL");
 require("dotenv").config({ path: "../config/.env" });
 
 module.exports = {
   getIndex: (req, res) => {
+    const api_key = process.env.TMDB_API_KEY;
+    console.log(api_key);
+
+    const getUpcoming = axios.get(BASE_URL + UPCOMING_MOVIES, {
+      params: {
+        api_key: api_key,
+      },
+    });
+    const getNowPlaying = axios.get(BASE_URL + NOW_PLAYING_MOVIES, {
+      params: {
+        api_key: api_key,
+      },
+    });
+
     axios
-      .get(`${BASE_URL}${NOW_PLAYING_MOVIES}${process.env.TMDB_API_KEY}`)
-      .then((data) => {
-        res.render("index.ejs", {
-          nowPlaying: data.data.results,
-          imageURL: IMAGE_URL,
-        });
-      })
-      .catch((err) => console.log(err));
+      .all([getUpcoming, getNowPlaying])
+      .then(
+        axios.spread((...results) => {
+          res.render("index.ejs", {
+            upComing: results[0].data.results,
+            nowPlaying: results[1].data.results,
+            imageURL: IMAGE_URL,
+          });
+        })
+      )
+      .catch((err) => {
+        console.log(err);
+      });
   },
 };
