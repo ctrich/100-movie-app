892cb13d57de16ca837430bd3f5396759b8d7308
ctrich <ctrich86@yahoo.com>
2022-09-26 20:36:30 -0700

    styled details page for larger screens

--- controllers/details.js
+++ controllers/details.js
@@ -3,12 +3,12 @@ const { BASE_URL, MOVIE_SEARCH, TV_SEARCH, IMAGE_URL } = require("../config/URL"
 require("dotenv").config({ path: "../config/.env" });
 
 module.exports = {
-  getMovie: (req, res) => {
+    getMovie: (req, res) => {
         const credits = axios.get(BASE_URL + "movie/" + req.params.id + "/credits?",{
             params: {
                 api_key: process.env.TMDB_API_KEY,
-        },
-      });
+            },
+        });
         const movieSearch = axios.get(BASE_URL + MOVIE_SEARCH + req.params.id + '?', {
             params: {
                 api_key: process.env.TMDB_API_KEY,
@@ -49,15 +49,49 @@ module.exports = {
             });
     },
 
-  getTv: (req, res) => {
-    console.log(req.params.id);
-    axios.get(BASE_URL + TV_SEARCH, {
+    getTv: (req, res) => {
+        const credits = axios.get(BASE_URL + "tv/" + req.params.id + "/credits?",{
             params: {
                 api_key: process.env.TMDB_API_KEY,
             },
-        }).then(results => {    
-            console.log(results.data);
-            res.render("details.ejs", { results: results.data, imageURL: IMAGE_URL })
-        }).catch(err => console.error(err))
-  },
+        });
+        const tvSearch = axios.get(BASE_URL + TV_SEARCH + req.params.id + '?', {
+            params: {
+                api_key: process.env.TMDB_API_KEY,
+            },
+        });
+        const providers = axios.get(BASE_URL + "tv/" + req.params.id + "/watch/providers?", {
+            params: {
+                api_key: process.env.TMDB_API_KEY,
+            },
+        });
+        const trailers = axios.get(BASE_URL + "tv/" + req.params.id + "/videos?", {
+            params: {
+                api_key: process.env.TMDB_API_KEY,
+            },
+        });
+        const similar = axios.get(BASE_URL + "tv/" + req.params.id + "/similar?", {
+            params: {
+                api_key: process.env.TMDB_API_KEY,
+            },
+        });
+
+        axios
+            .all([tvSearch, credits, providers, trailers, similar])
+            .then(
+        axios.spread((...results) => {
+            console.log(results[0].data)
+                res.render("details.ejs",{
+                    results: results[0].data,
+                    credits: results[1].data.crew.filter(crew => crew.job === "Director"),
+                    providers: results[2].data.results.US,
+                    trailers: results[3].data.results.slice(0, 3),
+                    similar: results[4].data.results,
+        imageURL: IMAGE_URL,
+                });
+            })
+            ).catch(err => {
+                console.log(err);
+            });
+    },
 };
